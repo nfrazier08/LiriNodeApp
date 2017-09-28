189b29595483f47a8787bf4e66622c04b5660f84
neliza <nicolasaelf64@gmail.com>
2017-09-28 13:57:03 -0600

    some stuff is kind of working

--- liri.js
+++ liri.js
@@ -3,7 +3,12 @@
     var twitPackage = require("twitter");
     var spotifyAPI = require("node-spotify-api");
     var requestAPI = require("request");
-    
+    var inquirer = require('inquirer');
+  
+    var command = process.argv[2];
+    var userChoice = process.argv[3];
+
+    //TWITTER!!!!!!
     //Import keys from the keys.js
     var twitterConfig = require("./keys.js");
 
@@ -17,23 +22,35 @@
         count: 20,
     };
 
-    // T.get('statuses/user_timeline', params, function(error, tweets, response) {
-    //   if (!error) {
-    //     console.log(tweets);
-    //   }
-    // });
-
-    T.get('statuses/user_timeline', params, function(error, tweets, response){
-        // if (!error){
-        console.log(tweets);
-        // console.log(tweets[0].text); //This works, but for only one of the tweets
-        //Create a for loop 
-        for (var i=0; i < response.length; i++){
-            for (var text in tweets[i]) {
-                console.log(tweet);
-            }
-        }
-    })
-        // }
-    // })
+    //SPOTIFY!!!!!!!
+    var spotify = new Spotify({
+     id: '14667e30ccc945ffaec23fbeca690091',
+     secret: '1cd9b6f86ff147529b8a82a4a66b0568'
+    });
     
+    if(command === "myTweets"){
+        T.get('statuses/user_timeline', params, function(error, tweets, response){
+            // if (!error){
+            // console.log(text);
+            console.log(tweets[0].text); //This works, but for only one of the tweets
+            //Create a for loop 
+            // for (var i=0; i < response.length; i++){
+            //     console.log(response)
+            //     for (var text in response[i]) {
+            //        console.log("This is text " + text)
+            //     }
+            // }
+        })
+    } 
+
+    if(command === "spotify-this-song"){
+        spotify.search({ type: 'track', query: userChoice }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            }
+           
+          console.log(data); 
+          });
+    }
+   
+      
