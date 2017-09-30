6fd61c7ac25ac824f9f7d02726be9013426efc69
neliza <nicolasaelf64@gmail.com>
2017-09-29 18:31:24 -0600

    Thank god, something is working

--- keys.js
+++ keys.js
@@ -1,18 +1,23 @@
 console.log('this is loaded');
 
 
-var twitterKeys =  {
-    consumer_key: 'WYEaSrFgHu0d1aIdEKKocDkUn',
-    consumer_secret: '2iKWT0vs6euy94wVwy2g9d6ssIZPz4TDqTxhjy2zuDFHYibqf6',
-    access_token_key: '913080205081591809-Dd8bm6u1YTdhvIiWlMBO3jlTr3F1NQV',
-    access_token_secret: 'lhQ5Rw3LyfzsdYe6sOfE4PuvpeQJh9xwvB33xeLBxfm3q',
-  };
+var myTwitterKeys ={
+    consumer_key: 'usk1Qum9GOhNoAqqrTu9Y3oYC', 
+    consumer_secret: '2uvnul4uv1DClLRHALOQvqlRuYjA6Gh3sVY9d3kvBCRNOu9JgF', 
+    access_token_key: '913910948460584960-VcnJSzqjnruMchLtISCub1I5EgbcMir', 
+    access_token_secret: '7J4eujAL1vuzelrJLz7uA74B8BNlY2Db7RU3GJmQSpSWl', 
 
-  module.exports = twitterKeys;
+}
 
-  // var spotifyKeys = {
-  //   id: '14667e30ccc945ffaec23fbeca690091',
-  //   secret: '1cd9b6f86ff147529b8a82a4a66b0568',
-  // }
+  var spotifyKeys = {
+    id: '14667e30ccc945ffaec23fbeca690091',
+    secret: '1cd9b6f86ff147529b8a82a4a66b0568',
+  }
 
-  // module.exports = spotifyKeys;
+  module.exports = {
+    tKeys: myTwitterKeys,
+    sKeys: spotifyKeys
+  }
+
+  
+
--- liri.js
+++ liri.js
@@ -1,53 +1,65 @@
 
     //Call in installed packages
-    var twitPackage = require("twitter");
-    var spotifyPackage = require("node-spotify-api");
+    var twitter = require("twitter");
+    var spotify = require("node-spotify-api");
     var requestAPI = require("request");
     var inquirer = require('inquirer');
+    //This is used to require in the random file for the do-what it says option
+    var fs = require("fs");
+
+    //Pull in file where twitter and spotify keys are saved
+    var appKeys = require("./keys.js");
   
-    var command = process.argv[2];
-    var userChoice = process.argv[3];
 
+//    inquirer.prompt([
+//         {
+//             //Set of choice of things for user to do:
+//             type: "list",
+//             name: "app",
+//             message: "Explore an app! Choose 1:",
+//             choices: [  "myTweets",
+//                         "spotify-this-song",
+//                         "movie-this",
+//                         "do-what-it-says"]
+//             }
+//    ]).then(function(inquirerResponse){
+//         //Define what happens with each of the choices
+//         if(inquirerResponse.app === "myTweets"){
+//             console.log('You chose to see my tweets');
+//             // printTweets ();          
+//         } else if(inquirerResponse.app === "spotify-this-song"){
+//             console.log("You want to hear some music");
+//             // printSongInfoFromSpotify();
+//         } else if(inquirerResponse.app === "movie-this"){
+//             console.log("Get some cool movie info!");
+//         } else if(inquirerResponse.app === "do-what-it-says"){
+//             // console.log("you are going to do something else")
+//             fs.readFile("random.txt", "utf8", function(error, data){
+//                 if(err){
+//                    console.log("you are going to do something else");
+//                    //This will default spotify the song "I want it that way" 
+//                 }
+//             })
+//         }
+//    }) //End of .then 
 
-    //TWITTER!!!!!!
-    //Import keys from the keys.js
-    var twitterConfig = require("./keys.js");
-
-    //New twitter variable
-    // Object of twitPackage that we can call functions inside
-    var T = new twitPackage(twitterConfig)
-
-    var params = {
-        screen_name: 'nfrazierDevelop',
-        // screen_name: 'nodejs',
-        count: 20,
-    };
-
-    //SPOTIFY!!!!!!!
-    // var spotifyConfig = require("./keys");
-
-    // var S = new spotifyPackage(spotifyConfig);
-    
-    if(command === "myTweets"){
-        T.get('statuses/user_timeline', params, function(error, tweets, response){
-            // if (!error){
-            // console.log(tweets); //This works, but for only one of the tweets
-            // console.log(tweets[0].text)
-            for(var i = 0; i <= tweets.length; i++) {
-                console.log(tweets[i].text)
+   //Functions for apps
+   function printTweets() {
+    //    console.log(appKeys.tKeys); //This works, we are calling the property  
+       var client = new twitter(appKeys.tKeys);     
+    //    console.log(client); //I am getting what I want HERE!!!
+        var params = {
+            screen_name: 'nicoleelfra',
+            count: 20
+        }
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (!error) {
+                // console.log(tweets); //This works!! I am getting my tweets object
+                for(i = 0; i < tweets.length; i++){
+                    console.log(tweets[i].text);
+                }
             }
-                
         })
-    } 
+   }
 
-    // if(command === "spotify-this-song"){
-    //     S.search({ type: 'track', query: userChoice}, function(err, data) {
-    //         if (err) {
-    //           return console.log('Error occurred: ' + err);
-    //         }
-           
-    //       console.log(JSON.stringify(data.tracks.items[0], null,2)); 
-    //       });
-    // }
-   
-      
+   printTweets();
