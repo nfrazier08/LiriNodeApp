b96fa02e6b3a710533dbdbab2cf938e083d6e967
neliza <nicolasaelf64@gmail.com>
2017-09-29 22:54:02 -0600

    ehhh

--- keys.js
+++ keys.js
@@ -6,17 +6,16 @@ var myTwitterKeys ={
     consumer_secret: '2uvnul4uv1DClLRHALOQvqlRuYjA6Gh3sVY9d3kvBCRNOu9JgF', 
     access_token_key: '913910948460584960-VcnJSzqjnruMchLtISCub1I5EgbcMir', 
     access_token_secret: '7J4eujAL1vuzelrJLz7uA74B8BNlY2Db7RU3GJmQSpSWl', 
-
 }
 
-  var spotifyKeys = {
+  var mySpotifyKeys = {
     id: '14667e30ccc945ffaec23fbeca690091',
     secret: '1cd9b6f86ff147529b8a82a4a66b0568',
   }
 
   module.exports = {
     tKeys: myTwitterKeys,
-    sKeys: spotifyKeys
+    sKeys: mySpotifyKeys,
   }
 
   
--- liri.js
+++ liri.js
@@ -6,42 +6,55 @@
     var inquirer = require('inquirer');
     //This is used to require in the random file for the do-what it says option
     var fs = require("fs");
-
     //Pull in file where twitter and spotify keys are saved
     var appKeys = require("./keys.js");
   
 
-//    inquirer.prompt([
-//         {
-//             //Set of choice of things for user to do:
-//             type: "list",
-//             name: "app",
-//             message: "Explore an app! Choose 1:",
-//             choices: [  "myTweets",
-//                         "spotify-this-song",
-//                         "movie-this",
-//                         "do-what-it-says"]
-//             }
-//    ]).then(function(inquirerResponse){
-//         //Define what happens with each of the choices
-//         if(inquirerResponse.app === "myTweets"){
-//             console.log('You chose to see my tweets');
-//             // printTweets ();          
-//         } else if(inquirerResponse.app === "spotify-this-song"){
-//             console.log("You want to hear some music");
-//             // printSongInfoFromSpotify();
-//         } else if(inquirerResponse.app === "movie-this"){
-//             console.log("Get some cool movie info!");
-//         } else if(inquirerResponse.app === "do-what-it-says"){
-//             // console.log("you are going to do something else")
-//             fs.readFile("random.txt", "utf8", function(error, data){
-//                 if(err){
-//                    console.log("you are going to do something else");
-//                    //This will default spotify the song "I want it that way" 
-//                 }
-//             })
-//         }
-//    }) //End of .then 
+   inquirer.prompt([
+        {
+            //Set of choice of things for user to do:
+            type: "list",
+            message: "Explore an app! Choose 1:",
+            choices: [  "myTweets",
+                        "spotify-this-song",
+                        "movie-this",
+                        "do-what-it-says"],
+            name: "app",
+            }
+   ]).then(function(inquirerResponse){
+        //Define what happens with each of the choices
+        if(inquirerResponse.app === "myTweets"){
+            console.log('You chose to see my tweets');
+            //Call the function that will print my tweets to the console
+            printTweets ();  
+        } else if(inquirerResponse.app === "spotify-this-song"){
+            console.log("You want to hear some music");
+            //Set up another inquirer function for the user to input song info 
+            inquirer.prompt([
+                {
+                    type:"input",
+                    message: "choose a song, any song!",
+                    name: "song"
+                }
+            ]).then(function(songResponse){
+                var chosenSong = songResponse.song;
+                //call spotify song function and input song variable
+                console.log(chosenSong);
+                printSongInfoFromSpotify(chosenSong);
+            })
+            // printSongInfoFromSpotify();
+        } else if(inquirerResponse.app === "movie-this"){
+            console.log("Get some cool movie info!");
+        } else if(inquirerResponse.app === "do-what-it-says"){
+            // console.log("you are going to do something else")
+            fs.readFile("random.txt", "utf8", function(error, data){
+                if(err){
+                   console.log("you are going to do something else");
+                   //This will default spotify the song "I want it that way" 
+                }
+            })
+        }
+   }) //End of .then 
 
    //Functions for apps
    function printTweets() {
@@ -64,4 +77,32 @@
         })
    }
 
-   printTweets();
+//    printTweets(); //THIS WORKS!!! YAYAYAYAYAY! 
+
+
+function printSongInfoFromSpotify(chosenSong) {
+        // console.log(appKeys.sKeys);
+        var spotSong = new spotify(appKeys.sKeys);
+        spotSong.search({
+                type: 'track',
+                query: chosenSong,
+                limit: 1
+            }, 
+            function(err, data) {
+                if (err) {
+                   return console.log("Error is: " + err);
+                } else if(!err){
+                   console.log(data);
+                   console.log("artist " + data.tracks.items[0].name);
+                //    console.log("song name " + data.tracks.items[0].)
+                //    console.log("link to song " + data.tracks.items[0].uri)
+                //    console.log("album " + data.tracks.items[0].)
+                }
+            })     
+        }
+
+//    printSongInfoFromSpotify();
+   //Artist
+   //Song Name
+   //Preview Link to song
+   //Album
